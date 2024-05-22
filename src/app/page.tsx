0bbb34dbfdedee57ed7485e7a8da165504c5dbc4
Marcos Santos <marcosv.santos@gruposbf.com.br>
2024-05-22 13:33:43 -0300

    feat(download): resume

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
 
+"use client"
 import Content from "@/components/Content";
 import Profile from "@/components/Profile";
-import { Button, Card, Grid } from "@mui/material";
+import { Download } from "@mui/icons-material";
+import { Button, Card, createTheme, Grid, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#0052cc',
+    },
+    secondary: {
+      main: '#424242',
+    },
+  },
+});
 
 export default function Home() {
   return (
+    <ThemeProvider theme={theme}>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Card>
         <Grid container padding={2}>
@@ -15,8 +29,11 @@ export default function Home() {
             <Content />
           </Grid>
         </Grid>
-        <Button>Request Resume</Button>
+        <Grid padding={2} display={'flex'} justifyContent={'flex-end'}>
+          <Button variant="contained" color="secondary" startIcon={<Download />}>Request Resume</Button>
+        </Grid>
       </Card>
     </main>
+    </ThemeProvider>
   );
 }
