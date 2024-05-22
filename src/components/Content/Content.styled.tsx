0e3c356b202293aeaf260b4f4650765a3474c3d4
Marcos Santos <marcosv.santos@gruposbf.com.br>
2024-05-22 13:21:40 -0300

    feat(Content): finished content

---
+++ src/components/Content/Content.styled.tsx
@@ -0,0 +1,11 @@
+import { Grid } from "@mui/material";
+import styled from "styled-components";
+
+export const ContentStyled = styled(Grid)`
+    h5 {
+      color: #E57373
+    }
+    p {
+      color: #616161
+    }
+`;
--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,20 +1,33 @@
+"use client"
+
 import { Grid, Typography } from "@mui/material";
+import { ContentStyled } from "./Content.styled";
 
 function Content() {
   return ( 
-    <Grid container padding={1}>
-      <Typography variant="h4" gutterBottom>
+    <ContentStyled container paddingLeft={5} paddingRight={5} direction={'column'}>
+      <Typography paddingBottom={0} marginBottom={0} variant="h4" gutterBottom>
         Marcos Vinícius dos Santos
       </Typography>
-      <Typography variant="h5" gutterBottom>
+      <Typography variant="h5" paddingBottom={2}>
         Web developer
       </Typography>
 
-      <Typography variant="h6" gutterBottom>
+      <Typography padding={0} margin={0} variant="h6" gutterBottom>
         Skills
-
       </Typography>
-    </Grid>
+      <Typography paddingBottom={1} variant="body1">ReactJS, ES7, Layout Design, Node, Elixir</Typography>
+
+      <Typography variant="h6" gutterBottom>
+        Location
+      </Typography>
+      <Typography variant="body1">Imbituba - SC - Brazil</Typography>
+
+      <Typography variant="h6" gutterBottom>
+        Interests
+      </Typography>
+      <Typography variant="body1">Frontend, backend, language paradigms, security information and surf 🌊</Typography>
+    </ContentStyled>
    );
 }
 
