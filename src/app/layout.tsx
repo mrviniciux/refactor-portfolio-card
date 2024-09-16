3c61eff05214895ee2834be3ece5003f28b380a7
mrviniciux <httpmarks@gmail.com>
2024-09-16 17:50:51 -0300

    feat(metadata): metadata tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,23 @@ import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
+const PUBLIC_URL = 'https://mrviniciux.netlify.app/';
+const meta = {
+  metadataBase: new URL(PUBLIC_URL),
   title: 'mrviniciux',
   description: 'Brazilian Frontend developer',
+  keywords: 'frontend, developer, programador, web',
+  robots: 'index, follow',
+};
+
+export const metadata: Metadata = {
+  ...meta,
+  openGraph: {
+    title: meta.title,
+    description: meta.description,
+    images: '/me.png',
+    url: PUBLIC_URL,
+  },
 };
 
 export default function RootLayout({
